cc2bfa168f5e36729a46bc12291f1bcf88c5766d
Martin Dawson <u1356770@gmail.com>
2018-06-20 21:23:11 +0200

    added build to .gitignore

--- onboardor/Components/footer/imprint.js
+++ onboardor/Components/footer/imprint.js
@@ -12,8 +12,8 @@ const Imprint = () => (
     </Helmet>
     <div className={styles.imprintContent}>
       <h1>Imprint</h1>
-      <div>Company Name: VAST NETWORK LTD</div>
-      <div>CEO: Martin Dawson</div>
+      <div>Company Name: FOZEN LIMITED</div>
+      <div>Company Director: Martin Dawson</div>
       <div>Address: 26 CalderView, Rastrick, Brighouse, HD63DQ United Kingdom</div>
       <div>Telephone: +(44)7473591097</div>
       <div>Email address: u1356770@gmail.com</div>
