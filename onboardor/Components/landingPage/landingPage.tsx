2d76ebc99e029587077ee347670c24a52ff25101
Martin Dawson <u1356770@gmail.com>
2018-10-24 23:39:34 +0200

    updated landing page

--- onboardor/Components/landingPage/landingPage.tsx
+++ onboardor/Components/landingPage/landingPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Text, Absolute, Relative } from "rebass";
+import { Container, Text, Absolute, Relative, Image} from "rebass";
 import { Box, Flex } from "grid-styled";
 import SubscribeMailingList from "../shared/form/subscribeMailingListContainer";
 import styled from "styled-components";
@@ -9,6 +9,9 @@ import Check from "../../wwwroot/assets/check.svg";
 import StepNumber from "./stepNumber";
 import DotLines from "../../wwwroot/assets/dotLines.svg";
 import Hide from "../shared/utilities/hide";
+import StepOne from "../../wwwroot/assets/step1.jpg";
+import StepTwo from "../../wwwroot/assets/step2.jpg";
+import StepThree from "../../wwwroot/assets/step3.jpg";
 
 const GreenCheck = styled(Check)`
   margin-right: 10px;
@@ -25,14 +28,14 @@ const StyledStepFlex = styled(Flex)`
 `;
 
 const StepFlex = ({ ...props }) => <StyledStepFlex {...props} mb={[40, 40, 0]} justifyContent="space-between" alignItems={["center", "center", "stretch"]} flexDirection={["column", "column", "row"]} />;
-const StepContainer = ({ ...props }) => <Box {...props} width={[1, 1, 370]} mt={[0, 0, 44]} order={[-1, -1, 0]} />;
+const StepContainer = ({ ...props }) => <Box {...props} width={[1, 1, 370]} mt={[0, 0, 44]} mb={[30, 30, 0]} order={[-1, -1, 0]} />;
 const StepHeading = ({ ...props }) => <Text {...props} textAlign={["center", "center", "left"]} mb={35} mt={25} fontSize={30} />;
 const StepContent = ({ ...props }) => <Box is="ul" {...props} mx={["auto", "auto", "none"]} fontSize={18} />;
 const StepItem = ({ children, ...props }: { children: string}) => (
   <Box is="li" width={["initial", "60%", "100%"]} mx={["5%", "20%", 0]} my={12} {...props}>
     <Flex>
       <GreenCheck width={20} height={20} />
-      <Text color="white" fontSize={21}>{children}</Text>
+      <Text fontSize={21}>{children}</Text>
     </Flex>
   </Box>
 );
@@ -58,14 +61,14 @@ const LandingPage = () => (
       </Text>
       <Box mt={50} mb={300}>
         <StepFlex>
-          image
-          <StepContainer ml={[0, 0, 30]}>
+          <Image
+            src={StepOne}
+          />
+          <StepContainer>
             <StepNumber>1</StepNumber>
-            <StepHeading>Easily create your teams on-boarding process directly through GitHub</StepHeading>
+            <StepHeading>Install the chrome onboardor extension</StepHeading>
             <StepContent>
-              <StepItem>30 second setup</StepItem>
-              <StepItem>Automatically integrates your team members</StepItem>
-              <StepItem>Built for developers, not HR teams</StepItem>
+              <StepItem>Do it all through GitHub. No external website.</StepItem>
             </StepContent>
           </StepContainer>
         </StepFlex>
@@ -73,30 +76,36 @@ const LandingPage = () => (
         <StepFlex>
           <StepContainer >
             <StepNumber>2</StepNumber>
-            <StepHeading>Measure and improve your on-boarding with analytics</StepHeading>
+            <StepHeading>Authorize any organizations that you want to onboard your members for</StepHeading>
             <StepContent>
-              <StepItem>Get automated regular feedback from your developers</StepItem>
-              <StepItem>A/B test your on-boarding to find what works</StepItem>
-              <StepItem>Tracks your developers progress</StepItem>
+              <StepItem>Automatically pulls in your organizations and team members instantly.</StepItem>
+              <StepItem>30 second setup.</StepItem>
             </StepContent>
           </StepContainer>
-          image
+          <Image
+            src={StepTwo}
+          />
         </StepFlex>
         <Hide sm={true} md={true}><DotLinesImage style={{ transform: "rotateX(180deg)" }} /></Hide>
         <StepFlex>
-          image
+          <Image
+            src={StepThree}
+          />
           <StepContainer>
             <StepNumber>3</StepNumber>
-            <StepHeading>Gain a 50%+ retention rate for new developers</StepHeading>
-            <StepContent>
-              <StepItem>Justifies your on-boarding process with actual data. No guessing!</StepItem>
-              <StepItem>Increases the rate at which your developers reach productivity</StepItem>
-              <StepItem>Applies on-boarding best practices for your teams</StepItem>
-            </StepContent>
+            <StepHeading>Onboard your new team members directly through GitHub</StepHeading>
+            <StepItem>Create an onboarding process template for each organization.</StepItem>
+            <StepItem>Save your onboarding processes for different teams, i.e backend team, frontend team.</StepItem>
+            <StepItem>Apply your saved onboarding processes to new team members in one click.</StepItem>
           </StepContainer>
         </StepFlex>
       </Box>
-      <SubscribeMailingList />
+      <SubscribeMailingList
+        subscribeText="Install Extension"
+        subscribeOnClick={() =>
+          window.open(`https://chrome.google.com/webstore/detail/onboardor/${process.env.EXTENSION_ID}`, "_blank")
+        }
+      />
     </Container>
     <Absolute style={{ width: "100%", height: 300, bottom: "-4%" }} zIndex={-1}>
       <BackgroundBlue height="100%" width="100%" style={{ transform: "rotateX(180deg)" }} />
